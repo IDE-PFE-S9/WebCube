5ca3533a51c7b8299a96d05f5c1cb72a55ca2124
guilpier <erwan.guilpin@reseau.eseo.fr>
2023-10-06 11:29:12 +0200

    Hover + selected all content

--- web-app/src/lib/TabManager/TabManagerItem.svelte
+++ web-app/src/lib/TabManager/TabManagerItem.svelte
@@ -25,10 +25,8 @@
 <!-- svelte-ignore a11y-click-events-have-key-events -->
 <!-- svelte-ignore a11y-no-static-element-interactions -->
 <div class="tab-item"  on:click={handleClick} class:selected={activeItem === name}>
-    <div class="file-item">
         <FileIcon fileExtension={fileExtension} />
         <div class="tab-item-name">{name}</div>
-    </div>
     <div class="delete-icon" on:click={handleDelete} class:selected={activeItem === name}>
         <svelte:component this={DeleteIcon} />
     </div>
@@ -40,33 +38,52 @@
         flex-direction: row;
         align-items: center;
         padding: 0 10px;
-        justify-content: center;
         cursor: pointer;
         color: rgb(136,136,136);
+        border-left: 1px solid rgb(41,41,41);
+        border-right: 1px solid rgb(41,41,41);
 
         &.selected {
+            border-top: 3px solid rgb(54, 117, 182);
             background-color: rgb(30,30,30);
             color: white;
+
+            .delete-icon{
+                fill: white;
+            }
         }
 
         &:hover{
             background-color: rgb(30,30,30);
+
+            .delete-icon{
+                fill: rgb(158,158,158);
+            }
         }
     }
 
-    .file-item{
-        display: flex;
-        flex-direction: row;
-        align-items: center; 
-    }
-
     .tab-item-name {
-        margin: 0 5px;
+        display:flex;
+        justify-content: center;
+        align-items: center;
+        margin: 0 10px;
     }
 
-    .delete-icon {
+    .delete-icon { 
         display: flex;
-        align-items: center; 
+        align-items: center;
+        fill: rgb(45,45,45);
+        
+        &:hover{
+            width: 1.5rem;
+            height: 1.5rem;
+            background-color: rgb(60,60,60);
+            border-radius: 5px;
+
+            &.selected{
+                background-color: (50,50,50);
+            }
+        }
     }
 
 </style>
