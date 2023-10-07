14fa9e6039eccee4efe1700c9154ed3c16b251e5
theolurat <theo.lurat@gmail.com>
2023-10-07 14:58:55 +0200

    feat: LSP server container

---
+++ LSP/README.md
@@ -0,0 +1,11 @@
+Dockerfile for building a Docker container of [eclipse/eclipse.jdt.ls](https://github.com/eclipse-jdtls/eclipse.jdt.ls)
+
+Fork of https://github.com/Kaylebor/eclipse.jdt.ls-docker
+
+[Built images are available here](https://hub.docker.com/r/theolrt/eclipse.jdt.ls)
+
+Running on OpenJDK 17
+
+Example run configuration:
+
+        docker run -d theolrt/eclipse.jdt.ls:<version>
---
+++ LSP/start.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+launcher_file_name=$(find ./plugins -name "org.eclipse.equinox.launcher_*.jar" | sort | head -n 1)
+
+java \
+	-Declipse.application=org.eclipse.jdt.ls.core.id1 \
+	-Dosgi.bundles.defaultStartLevel=4 \
+	-Declipse.product=org.eclipse.jdt.ls.core.product \
+	-Dlog.level=ALL \
+	-Dosgi.dev \
+ 	-Dsocket.stream.debug=true \
+	-Xmx1G \
+	-DCLIENT_PORT=5036 \
+	-DCLIENT_HOST=0.0.0.0 \
+	--add-modules=ALL-SYSTEM \
+	--add-opens java.base/java.util=ALL-UNNAMED \
+	-jar $launcher_file_name \
+	-configuration ./config_linux \
+	-data $ECLIPSE_WORKSPACE \
