fd22c672d2a385ea95036fc1e76f4e10bb808817
guilpier <erwan.guilpin@reseau.eseo.fr>
2023-11-29 16:49:45 +0100

    Fix CSS

--- web-app/src/lib/PopUps/popup.js
+++ web-app/src/lib/PopUps/popup.js
@@ -6,8 +6,9 @@ export function showDeniedPopup() {
         title: "Vous devez d'abord sélectionner une archive",
         icon: 'warning',
         background: "#1e1e1e",
-        color: "white",
+        color: "rgb(187, 187, 187)",
         allowOutsideClick: true,
+        confirmButtonColor: "rgb(52,120,198)"
     });
 }
 
@@ -15,9 +16,9 @@ export function showLoginPopup() {
     Swal.fire({
         title: "Vous devez d'abord vous connecter",
         icon: 'warning',
-        html: '<button id="loginBtn" style="cursor:pointer;color:white;font-size:1rem;background-color:#1e1e1e;width:40%;height:50px;border-radius:10px;border:1px solid white">Se connecter</button>',
+        html: '<button id="loginBtn" style="cursor:pointer;color:rgb(187, 187, 187);font-size:1rem;background-color:#1e1e1e;width:40%;height:50px;border-radius:10px;border:1px solid rgb(52,120,198)">Se connecter</button>',
         background: "#1e1e1e",
-        color: "white",
+        color: "rgb(187, 187, 187)",
         showConfirmButton: false,
         allowOutsideClick: false,
         allowEscapeKey: false,
