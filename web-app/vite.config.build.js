38cfd014c3d8fe81320325cfbc8416a61ab92529
theolurat <theo.lurat@gmail.com>
2024-01-22 12:12:59 +0100

    fix: somes basics prods versatility

--- web-app/vite.config.build.js
+++ web-app/vite.config.build.js
@@ -4,9 +4,9 @@ import { defineConfig } from 'vite';
 export default defineConfig({
 	plugins: [sveltekit()],
 	define: {
-		'process.env.API_URL': JSON.stringify('https://172.24.6.27'),
-		'process.env.WS_URL': JSON.stringify('ws://172.24.6.27'),
+		'process.env.API_URL': JSON.stringify('https://172.24.6.43'),
+		'process.env.WS_URL': JSON.stringify('ws://172.24.6.43'),
 		'process.env.PROJECT_PATH': JSON.stringify('/api/code'),
-		'process.env.CLIENT_HOST': JSON.stringify('https://172.24.6.27')
+		'process.env.CLIENT_HOST': JSON.stringify('https://172.24.6.43')
 	}
 });
