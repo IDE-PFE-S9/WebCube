2d7fbc6835e2de43b79c153fed0e06b14fa76219
ArthurMynl <61516506+ArthurMynl@users.noreply.github.com>
2024-01-24 22:26:00 +0100

    hotfix: change to wss

--- web-app/vite.config.build.js
+++ web-app/vite.config.build.js
@@ -5,7 +5,7 @@ export default defineConfig({
 	plugins: [sveltekit()],
 	define: {
 		'process.env.API_URL': JSON.stringify('https://172.24.6.43'),
-		'process.env.WS_URL': JSON.stringify('ws://172.24.6.43'),
+		'process.env.WS_URL': JSON.stringify('wss://172.24.6.43'),
 		'process.env.PROJECT_PATH': JSON.stringify('/api/code'),
 		'process.env.CLIENT_HOST': JSON.stringify('https://172.24.6.43')
 	}
